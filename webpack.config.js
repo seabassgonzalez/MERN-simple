d211bcf3f1329df30143a70199eb0d6b9714faf3
seabassgonzalez <seabassgonzalez@Sebastians-MacBook-Pro.local>
2017-05-27 18:59:49 -0700

    Initial webpack.config.js file using babel-loader es2015 react

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,28 @@
 	// 	module object instructions for transpiling
 		// 	loaders array containing object
 			// 	test to catch js and jsx files
-			// 	loader set to babel-loader
+			// 	loader set to string babel-loader
 			// 	exclude set to node modules
 			// 	query instructions for babel
 				// 	presets set to array es2015 and react
+
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+	entry: './app.js',
+	output: {
+		path: __dirname,
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [{
+			test: /.jsx?$/,
+			loader: 'babel-loader',
+			exclude: /node_modules/,
+			query:{
+				presets: ['es2015', 'react']
+			}
+		}]
+	}
+}
